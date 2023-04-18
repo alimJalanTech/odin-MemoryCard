54dd45292512a3b33e58a35fe201ae62fdc81db5
Alim <52186295+alimalim77@users.noreply.github.com>
2023-04-18 11:03:24 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import { useState } from "react";
 import UserHeader from "./components/header";
 import CharacterCard from "./components/characters";
 import { ContentStyle } from "./styled/style";
-
-import Wednesday from "./components/img/wednesday.jpg";
-import Erid from "./components/img/erid.jpg";
-import Thing from "./components/img/thing.jpg";
-import Tyler from "./components/img/tyler.jpg";
-import Xavier from "./components/img/xavier.jpg";
-import Larissa from "./components/img/larissa.jpg";
+import { characters } from "./data";
 
 type Character = {
   id?: number;
@@ -17,45 +11,12 @@ type Character = {
   name: string;
 };
 
-const characters: Character[] = [
-  {
-    id: 1,
-    name: "Wednesday",
-    image: Wednesday,
-  },
-  {
-    id: 2,
-    name: "Erid",
-    image: Erid,
-  },
-  {
-    id: 3,
-    name: "Tyler",
-    image: Tyler,
-  },
-  {
-    id: 4,
-    name: "Thing",
-    image: Thing,
-  },
-  {
-    id: 5,
-    name: "Xavier",
-    image: Xavier,
-  },
-  {
-    id: 6,
-    name: "Larissa",
-    image: Larissa,
-  },
-];
-
 function App() {
   const [bestScore, setBestScore] = useState(0);
   const [clicked, setClicked] = useState<number[]>([]);
   const [score, setScore] = useState(0);
 
-  function shuffle(array: Character[]) {
+  function shuffle(array: Character[]): void | Character[] {
     let currentIndex = array.length;
     let temporaryValue: Character;
     let randomIndex: number;
@@ -79,9 +40,9 @@ function App() {
     } else {
       const newClicked = [...clicked, id];
       setClicked(newClicked);
-      setScore(score + 1);
+      setScore((s) => s + 1);
       if (score + 1 > bestScore) {
-        setBestScore(score + 1);
+        setBestScore((s) => s + 1);
       }
     }
     shuffle(characters);
